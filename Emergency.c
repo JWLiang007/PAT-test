2268f659bca67f9cd02fd474cd81713e22d30d80
unknown <961055871@qq.com>
2021-01-04 23:55:06 +0800

    sync

--- Emergency.c
+++ Emergency.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int main()
 {
@@ -11,12 +12,22 @@ int main()
     {
         scanf("%d", team + i);
     }
-    int dist[nNode][nNode];
+    int dist[nNode][nNode] ;
+    for (size_t i = 0; i < nNode; i++)
+    {
+        for (size_t j = 0; j < nNode; j++)
+        {
+            if(i==j) dist[i][j]=0;
+            else dist[i][j]=INT_MAX;
+        }
+        
+    }
+    
     int s,t,l;
     for (size_t i = 0; i < nPath; i++)
     {
         scanf("%d %d %d",&s,&t,&l);
-        
+        dist[s][t]=l;
     }
 
     return 0;
