4be596d7e6655fdbfe335c4ff19b21c2d9b40eb2
JWLiang <961055871@qq.com>
2021-01-07 20:03:57 +0800

    sync

--- Emergency.c
+++ Emergency.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <limits.h>
 
+typedef struct record
+{
+    int maxTeam;
+    int minCost;
+    int numOfminCost;
+} record;
+
 int main()
 {
 
+    printf("%d",INT_MAX+10);
     int nNode, nPath, source, dest, *team;
     scanf("%d %d %d %d", &nNode, &nPath, &source, &dest);
     team = (int *)malloc(nNode * sizeof(int));
@@ -12,23 +20,30 @@ int main()
     {
         scanf("%d", team + i);
     }
-    int dist[nNode][nNode] ;
+    int dist[nNode][nNode];
     for (size_t i = 0; i < nNode; i++)
     {
         for (size_t j = 0; j < nNode; j++)
         {
-            if(i==j) dist[i][j]=0;
-            else dist[i][j]=INT_MAX;
+            if (i == j)
+                dist[i][j] = 0;
+            else
+                dist[i][j] = INT_MAX;
         }
-        
     }
-    
-    int s,t,l;
+    int s, t, l;
     for (size_t i = 0; i < nPath; i++)
     {
-        scanf("%d %d %d",&s,&t,&l);
-        dist[s][t]=l;
+        scanf("%d %d %d", &s, &t, &l);
+        dist[s][t] = l;
     }
 
-    return 0;
+    record *result = (record *)malloc(sizeof(record));
+    result->maxTeam=team[source];
+    result->minCost=dist[source][dest];
+    result->numOfminCost=1;
+
+
+
+        return 0;
 }
