536d8d74454e3b2b859115a484ee3c8c07a1e601
unknown <961055871@qq.com>
2021-01-17 16:27:09 +0800

    sync

--- CountLeaves.c
+++ CountLeaves.c
@@ -1,45 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct node
+#define MAXNODES 101
+
+struct Node
 {
-    char nNo[2];
-    struct node *child;
     int numOfChild;
+    int *children;
     int level;
-} node;
+}nodes[MAXNODES];
 
-int main()
-{
-    int numOfNodes, numOfNonLeafNodes = 0;
-    node *root;
-    scanf("%d %d\n", &numOfNodes, &numOfNonLeafNodes);
-    // 读取到非叶子节点为0，直接返回
-    if (numOfNonLeafNodes == 0)
+
+int main(){
+    int n, m, parentNo, numOfChild,*children;
+    scanf("%d %d", &n, &m);
+    if(n==0)
         return 0;
-
-    for (int i = 0; i < numOfNonLeafNodes; i++)
-    {
-        // 初始化非叶节点
-        node* nonLeafNode=(node*)malloc(sizeof(node));
-        // char pNo[2];
-        // fgets 读入时末尾带空字符
-        // fgets(a,3,stdin);
-        int numOfChild;
-        scanf("%c%c %d",nonLeafNode->nNo,nonLeafNode->nNo+1,&nonLeafNode->numOfChild);
-        // nonLeafNode->nNo[0]=pNo[0];
-        // nonLeafNode->nNo[1]=pNo[1];
-        // nonLeafNode->numOfChild=numOfChild;
-        if(nonLeafNode->nNo[0]=='0' && nonLeafNode->nNo[1]=='1')
-            root=nonLeafNode;
-
-        // 初始化孩子节点
-        for(int j = 0; j < numOfChild; j++){
-            // char cNo[2];
-            node* cNode=(node*)malloc(sizeof(node));
-            scanf(" %c%c",cNode->nNo,cNode->nNo+1);
-            cNode->child=NULL;
-        }
-
+    if(m==0){
+        printf("%d",n);
+        return 0;
     }
+    for(int i = 0; i < MAXNODES; i++){
+        nodes[i].numOfChild=0;
+    }
+    for (int i = 0; i < m ;i++){
+        scanf("%d %d", &parentNo, &numOfChild);
+        struct Node *parentNode = nodes+parentNo;
+        parentNode->numOfChild = numOfChild;
+        children = parentNode->children=(int *)malloc(numOfChild*sizeof(int));
+        for (int j = 0; j < numOfChild; j++){
+            scanf("%d",children+j);
+        }
+    }
+    
+    int queue[MAXNODES],s=0,t=0,result[MAXNODES] = {0},maxLevel=0;
+    nodes[1].level=0;
+    queue[t++]=1;
+    struct Node *pNode, *cNode=NULL;
+    while(s != t){
+        pNode = nodes+queue[s++];
+        int childIndex;
+        for(int i = 0; i < pNode->numOfChild; i++){
+            childIndex = pNode->children[i];
+            cNode = nodes + childIndex;
+            cNode->level = pNode->level+1;
+            if(cNode->numOfChild ==0)
+                result[cNode->level]++;
+            else{
+                queue[t++]=childIndex;
+            }
+        }
+        if (cNode && cNode->level > maxLevel) 
+            maxLevel = cNode->level;
+    }
+    for (int i = 0; i < maxLevel;i++)
+        printf("%d ",result[i]);
+    printf("%d",result[maxLevel]);
     return 0;
 }
