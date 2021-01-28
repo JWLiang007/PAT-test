4a3cf9aaee3c1edbc80e28d290b08fc9c53e78c4
JWLiang <961055871@qq.com>
2021-01-28 20:23:04 +0800

    sync

--- ProdOfPolyn.c
+++ ProdOfPolyn.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
+double fabs(double a);
 int getInput(int *count, double coefs[]);
 int main()
 {
-    int count_1, count_2, maxExp_1, maxExp_2,maxExp_Out;
+    int count_1, count_2, maxExp_1, maxExp_2, maxExp_Out, count = 0, ori;
     double coef_1[1001] = {0}, coef_2[1001] = {0}, coef_Out[2002] = {0};
     maxExp_1 = getInput(&count_1, coef_1);
     maxExp_2 = getInput(&count_2, coef_2);
@@ -11,14 +12,26 @@ int main()
     {
         for (int j = 0; j <= maxExp_2; j++)
         {
-            coef_Out[i+j]+=coef_1[i]*coef_2[j];
+            coef_Out[i + j] += coef_1[i] * coef_2[j];
         }
     }
-    maxExp_Out = maxExp_1*maxExp_2;
-    
-    
+    maxExp_Out = maxExp_1 + maxExp_2;
+    for (int i = maxExp_Out; i >=0;i--){
+        if(fabs(coef_Out[i])>1e-6){
+            count++;
+        }
+    }
+    printf("%d",count);
 
-    return 0;
+    for (int i = maxExp_Out; i >=0;i--){
+        if(fabs(coef_Out[i])>1e-6){
+            printf(" %d %.1f",i,coef_Out[i]);
+        }
+    }
+
+
+
+        return 0;
 }
 int getInput(int *count, double coefs[])
 {
@@ -32,4 +45,8 @@ int getInput(int *count, double coefs[])
             max = exp;
     }
     return max;
+}
+
+double fabs(double a){
+    return a>0?a:-a;
 }
---
+++ Radix.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    char digit1[10],digit2[10],radix;
+    int flag,;
+    return 0;
+}
