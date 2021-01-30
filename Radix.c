3fd00c0345dbd70a4cfc2c62b6befba1f96df299
unknown <961055871@qq.com>
2021-01-30 19:36:03 +0800

    sync

--- Radix.c
+++ Radix.c
@@ -19,42 +19,49 @@ int getDigFrmChar(char c)
 long long calDecm(char dig[], long long radix)
 {
     long long res = 0;
-    int n ;
+    int n;
     for (int i = 0; dig[i] != '\0'; i++)
     {
         n = getDigFrmChar(dig[i]);
-        if((LLONG_MAX - n) / radix < res) /* overflow */
+        if ((LLONG_MAX - n) / radix < res) /* overflow */
             return -1;
         res = res * radix + n;
-
     }
     return res;
-
 }
 
 long long binsearch(char *s, long long n, long long rmin, long long rmax)
 {
-    long long r, m;
-    while(rmax >= rmin)
+    long long r, m, ans = -1;
+    while (rmax >= rmin)
     {
-        r = rmin + (rmax - rmin) / 2; /* avoid (rmin + rmax) overflow */
-        if((m = calDecm(s, r)) > n || m == -1)
+        r = rmin + (rmax - rmin) / 2; 
+        if ((m = calDecm(s, r)) > n || m == -1)
             rmax = r - 1;
-        else if(m < n)
+        else if (m < n)
             rmin = r + 1;
         else
-            return r;
+        {
+            ans = r;
+            rmax = r - 1;
+        }
     }
-    return -1;
+    return ans;
 }
 
 int minradix(char *s)
-{   /* Simply the largest digit in the number plus 1 */
-    char r;
-    for(r = '0'; *s; s++)
-        if(*s > r)
-            r = *s;
-    return getDigFrmChar(r) + 1;
+{ 
+    int temp,ans=0;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        temp = getDigFrmChar(s[i]);
+        if (temp > ans)
+        {
+            ans = temp;
+        }
+    }
+    return (ans + 1);
+
 }
 
 int main()
@@ -69,35 +76,22 @@ int main()
         fir = digit1;
         sec = digit2;
     }
-    else if(flag == 2)
+    else if (flag == 2)
     {
         fir = digit2;
         sec = digit1;
     }
     num1 = calDecm(fir, radix);
-    // int s = 0, temp;
-    long long s = minradix(sec);     
-    long long t = LLONG_MAX;
-    // for (int i = 0; sec[i] != '\0'; i++)
-    // {
-    //     temp = getDigFrmChar(sec[i]);
-    //     if (temp > s)
-    //     {
-    //         s = temp;
-    //     }
-    // }
-    // s = s + 1;
 
-    if(strlen(sec) == 1){
-        if(num1 == (s - 1)) printf("%d",s);
-        else printf("%s","Impossible");
-    }else 
-    {
-        long long r = binsearch(sec,num1,s,t) ;
-        if(r != -1)       printf("%lld", r);
-        else                    printf("Impossible");
-    }
-    
+    long long s = minradix(sec);
+    long long t = num1 + 1;
+
+
+    long long r = binsearch(sec, num1, s, t);
+    if (r != -1)
+        printf("%lld", r);
+    else
+        printf("Impossible");
 
 
     return 0;
